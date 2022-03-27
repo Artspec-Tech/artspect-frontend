6af7f06189a462b1a84a1dad9600e7f77035cadf
Nonthapat Kaewamporn <tontan2545@gmail.com>
2022-03-27 16:08:32 +0700

    test no vercel build

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.ts!</a>
+          Welcome to <a href="https://nextjs.org">Next.ts! test no build</a>
         </h1>
 
         <p className={styles.description}>
