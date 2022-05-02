1c265d8f9db0eefb4ddca5ab924e72eb1308d375
Nonthapat Kaewamporn <tontan2545@gmail.com>
2022-05-02 17:14:26 +0700

    Use mui components for body

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import type { NextPage } from "next";
+import { Box } from "@mui/material";
 import Layout from "../components/layout";
+import { HStack } from "../components/common";
 
 const Home: NextPage = () => {
   return (
     <Layout>
-      <div>Hello, world!!!!</div>
+      <HStack flexGrow={1}>Hello, world!</HStack>
     </Layout>
   );
 };
