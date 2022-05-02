7114064cbd195432afe4df81322d001f94a9c904
Nonthapat Kaewamporn <tontan2545@gmail.com>
2022-05-02 20:31:21 +0700

    add footer image and bg image component

---
+++ components/common/BgImage.tsx
@@ -0,0 +1,46 @@
+import { Box, BoxProps, Typography } from "@mui/material";
+import Image from "next/image";
+import React from "react";
+import { FooterBackground } from "../../utils/images";
+import HStack from "./HStack";
+
+type Props = BoxProps & {
+  height?: string | number;
+  width?: string | number;
+};
+
+const BgImage = ({ height, width, children, ...props }: Props) => {
+  return (
+    <Box
+      style={{
+        alignItems: "center",
+        display: "flex",
+        justifyContent: "center",
+        position: "relative",
+        height: height,
+        width: "100%",
+      }}
+      {...props}
+    >
+      <Image
+        alt={"imgalt"}
+        src={FooterBackground}
+        layout="fill"
+        objectFit="cover"
+        quality={80}
+      />
+      {children && (
+        <HStack
+          justifyContent={"space-around"}
+          style={{
+            zIndex: 10,
+          }}
+        >
+          {children}
+        </HStack>
+      )}
+    </Box>
+  );
+};
+
+export default BgImage;
---
+++ utils/images.ts
@@ -0,0 +1,1 @@
+export * as FooterBackground from "../assets/images/footer-background.png";
