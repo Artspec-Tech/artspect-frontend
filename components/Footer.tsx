24ebdbbd4ad8f68f53c40551154dc1bfd2632972
Nonthapat Kaewamporn <tontan2545@gmail.com>
2022-05-02 20:31:31 +0700

    add bg image to footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,49 @@
 import React from "react";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import { IconButton } from "@mui/material";
+import { HStack, VStack } from "./common";
+import BgImage from "./common/BgImage";
 
 type Props = {};
 
 const Footer = (props: Props) => {
-  return <div>Footer</div>;
+  return (
+    <VStack>
+      <BgImage height={"450px"}>
+        <div
+          style={{
+            width: "45vw",
+            aspectRatio: "16/9",
+            backgroundColor: "#C4C4C4",
+          }}
+        />
+        <div
+          style={{
+            width: "45vw",
+            aspectRatio: "16/9",
+            backgroundColor: "#C4C4C4",
+          }}
+        />
+      </BgImage>
+      <HStack
+        paddingX={20}
+        height={"400px"}
+        justifyContent={"space-between"}
+        style={{
+          backgroundColor: `rgba(185, 223, 255, 0.47)`,
+        }}
+      >
+        <IconButton size="large" color="primary">
+          <ChevronLeftIcon fontSize="inherit" />
+        </IconButton>
+
+        <IconButton size="large" color="primary">
+          <ChevronRightIcon fontSize="inherit" />
+        </IconButton>
+      </HStack>
+    </VStack>
+  );
 };
 
 export default Footer;
