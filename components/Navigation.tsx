50208bfc0d0b5fb3a5fee2acd9e188edbbc3ca62
Nonthapat Kaewamporn <tontan2545@gmail.com>
2022-05-02 16:25:54 +0700

    navigation

---
+++ components/Navigation.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { HStack } from "./common";
+
+type Props = {};
+
+const Navigation = (props: Props) => {
+  return <HStack>Navigation</HStack>;
+};
+
+export default Navigation;
