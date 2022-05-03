0d3c14ffe222540bd25fa224d0e4ee4426c9f2b6
Nonthapat Kaewamporn <tontan2545@gmail.com>
2022-05-03 10:49:51 +0700

    fix typo

--- utils/images.ts
+++ utils/images.ts
@@ -1,2 +1,2 @@
 export * as FooterBackground from "../assets/images/footer-background.png";
-export * as VanGohImage from "../assets/images/van-goh.jpeg";
+export * as VanGoghImage from "../assets/images/van-gogh.jpeg";
