49924f12ee2a1fc1ab76c35b268c75551850dba8
Nonthapat Kaewamporn <tontan2545@gmail.com>
2022-05-20 21:51:43 +0700

    Finalized footer

---
+++ components/Layout/Footer/artists.ts
@@ -0,0 +1,18 @@
+import { VanGoghImage } from "@utils/images";
+
+const artists = [
+  {
+    img: VanGoghImage,
+    quote:
+      '"Paintings have a life of their own that derives from the painter\'s soul."',
+    name: "Vincent Van Gogh",
+  },
+  {
+    img: VanGoghImage,
+    quote:
+      '"Paintings have a life of their own that derives from the painter\'s soul."',
+    name: "Vincent Van Gogh",
+  },
+];
+
+export default artists;
--- components/Layout/Footer/index.tsx
+++ components/Layout/Footer/index.tsx
@@ -1,40 +1,13 @@
 import React from "react";
-import { VStack, Center, HStack } from "@components/common";
-import { PietileCarousel } from "pietile-carousel";
-import ArtistCarousel from "./ArtistCarousel";
+import { VStack } from "@components/common";
 import TwoImage from "./TwoImage";
-import { VanGoghImage } from "@utils/images";
-import Image, { StaticImageData } from "next/image";
-import { Box } from "@mui/system";
+import ArtistCarousel from "./ArtistCarousel";
 
-type Props = {};
-
-const Footer = (props: Props) => {
-  const carouselStyle = {
-    width: 150,
-    height: 100,
-  };
-
-  const itemStyle = {
-    width: "100%",
-    height: "100%",
-  };
-
-  const colors: string[] = ["red", "orange", "yellow"];
-
+const Footer = () => {
   return (
     <VStack>
       <TwoImage />
-      <PietileCarousel style={{ width: 400, height: 600 }}>
-        {colors.map((color) => (
-          <div
-            key={color}
-            style={{ width: "100%", height: "100%", backgroundColor: color }}
-          >
-            Hello
-          </div>
-        ))}
-      </PietileCarousel>
+      <ArtistCarousel />
     </VStack>
   );
 };
