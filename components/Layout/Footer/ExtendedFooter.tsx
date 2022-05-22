8be61f8ac0efb6a541863ac72bef67775edfe5d6
Lukyord <ttansiri524@gmail.com>
2022-05-22 20:53:08 +0700

    Add extended Footer template

---
+++ components/Layout/Footer/ExtendedFooter.tsx
@@ -0,0 +1,46 @@
+import { BgImage, HStack, VStack } from "@components/common";
+import { Box } from "@mui/system";
+import React from "react";
+
+type Props = {};
+
+const ExtendedFooter = (props: Props) => {
+  return (
+    <BgImage>
+      <HStack marginTop={"2rem"} justifyContent={"space-between"}>
+        <Box
+          style={{
+            width: "5%",
+          }}
+        >
+          IG
+        </Box>
+        <VStack width={"25%"} marginBottom={"2rem"}>
+          Artspect
+        </VStack>
+        <VStack width={"12%"} marginBottom={"2rem"}>
+          Contact Us
+        </VStack>
+        <VStack width={"10%"} marginBottom={"2rem"}>
+          Quick Links
+        </VStack>
+        <Box
+          style={{
+            width: "40%",
+            marginBottom: "2rem",
+          }}
+        >
+          <div
+            style={{
+              width: "35vw",
+              aspectRatio: "16/9",
+              backgroundColor: "#C4C4C4",
+            }}
+          />
+        </Box>
+      </HStack>
+    </BgImage>
+  );
+};
+
+export default ExtendedFooter;
--- components/Layout/Footer/index.tsx
+++ components/Layout/Footer/index.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import { VStack } from "@components/common";
 import TwoImage from "./TwoImage";
 import ArtistCarousel from "./ArtistCarousel";
+import ExtendedFooter from "./ExtendedFooter";
 
 const Footer = () => {
   return (
     <VStack>
       <TwoImage />
       <ArtistCarousel />
+      <ExtendedFooter />
     </VStack>
   );
 };
