ded37d8c5787372b0c27f7524a86632d1d98f058
Lukyord <ttansiri524@gmail.com>
2022-05-23 00:07:46 +0700

    finished configuration of extended footer

--- components/Layout/Footer/ExtendedFooter.tsx
+++ components/Layout/Footer/ExtendedFooter.tsx
@@ -16,118 +16,151 @@ const ExtendedFooter = (props: Props) => {
   const { pathname } = useRouter();
   const isCurrentPath = (path: string) => pathname === path;
   return (
-    <BgImage source={ExtendedFooterBackground}>
-      <HStack margin={"2rem"} justifyContent={"space-between"} height={"100%"}>
-        {/* <VStack width={"5%"} alignItems={"end "}>
+    <HStack
+      justifyContent={"space-between"}
+      height={"25rem"}
+      gap={2}
+      sx={{
+        backgroundColor: "#191323",
+      }}
+    >
+      {/* <VStack width={"5%"} alignItems={"end "}>
           <IconButton color="secondary">
             <InstagramIcon fontSize="large" />
           </IconButton>
         </VStack> */}
-        <VStack width={"35%"} height={"100%"} justifyContent={"space-between"}>
-          <Typography
-            fontSize={30}
-            fontWeight={fontWeights.semiBold}
-            color="white"
-            textAlign={"left"}
-            width={"100%"}
-          >
-            Artspect
-          </Typography>
-          <Typography
-            fontWeight={fontWeights.bold}
-            fontSize={15}
-            color="#F3C766"
-          >
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae
-            adipisci perferendis facere quia. Consequatur rem recusandae
-            consectetur sapiente earum aut repellat quibusdam, voluptatem nobis
-            quod nam eius suscipit, inventore amet?
-          </Typography>
-          <Typography fontSize={12} color="white">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic eius
-            distinctio blanditiis cumque doloribus vero delectus fugiat minima,
-            veniam nam modi eveniet cupiditate. Quia voluptas cupiditate
-            corrupti iste dolor officia.
-          </Typography>
-        </VStack>
-        <VStack width={"12%"}>
-          <Typography
-            fontWeight={fontWeights.bold}
-            fontSize={15}
-            color="#F3C766"
-          >
-            Contact Us
-          </Typography>
-          <Typography fontSize={12} color="white">
-            Phone: +66 96-930-53xx
-          </Typography>
-          <Typography fontSize={12} color="white">
-            Email: xxxxxx@gmail.com
-          </Typography>
-        </VStack>
-        <VStack width={"10%"}>
-          <Typography
-            fontWeight={fontWeights.bold}
-            fontSize={15}
-            color="#F3C766"
-          >
-            Quick Links
-          </Typography>
-          {navigations.map((nav) => (
-            <Link key={nav.path} href={nav.path} passHref>
-              <Box>
-                <Typography
-                  color="#FFFFFF"
-                  fontSize={15}
-                  component={motion.p}
-                  style={{
-                    cursor: "pointer",
-                  }}
-                  whileHover={{
-                    textShadow: "4px 4px 10px gray",
-                  }}
-                >
-                  {nav.text}
-                </Typography>
-                {isCurrentPath(nav.path) && (
-                  <motion.div
-                    layout
-                    layoutId="divider"
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: 1 }}
-                    transition={{
-                      duration: 0.4,
-                      ease: "easeOut",
-                    }}
-                    style={{
-                      position: "absolute",
-                      top: "100%",
-                      width: "110%",
-                    }}
-                  ></motion.div>
-                )}
-              </Box>
-            </Link>
-          ))}
-          <Typography fontSize={15} color="white">
-            My Account
-          </Typography>
-        </VStack>
-        <Box
+      <VStack
+        width={"35%"}
+        justifyContent={"space-around"}
+        height={"80%"}
+        marginLeft={"2rem"}
+      >
+        <Typography
+          fontSize={30}
+          fontWeight={fontWeights.semiBold}
+          color="white"
+          textAlign={"left"}
+          width={"100%"}
+        >
+          Artspect
+        </Typography>
+        <Typography fontWeight={fontWeights.bold} fontSize={15} color="#F3C766">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae
+          adipisci perferendis facere quia. Consequatur rem recusandae
+          consectetur sapiente earum aut repellat quibusdam, voluptatem nobis
+          quod nam eius suscipit, inventore amet?
+        </Typography>
+        <Typography
+          fontSize={12}
+          color="white"
           style={{
-            width: "40%",
+            opacity: "0.6",
           }}
         >
-          <div
-            style={{
-              width: "37vw",
-              aspectRatio: "16/9",
-              backgroundColor: "#C4C4C4",
-            }}
-          />
-        </Box>
-      </HStack>
-    </BgImage>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic eius
+          distinctio blanditiis cumque doloribus vero delectus fugiat minima,
+          veniam nam modi eveniet cupiditate. Quia voluptas cupiditate corrupti
+          iste dolor officia.
+        </Typography>
+      </VStack>
+      <VStack
+        width={"12%"}
+        justifyContent={"space-around"}
+        height={"40%"}
+        marginTop={"1rem"}
+      >
+        <Typography fontWeight={fontWeights.bold} fontSize={15} color="#F3C766">
+          Contact Us
+        </Typography>
+        <Typography
+          fontSize={12}
+          color="white"
+          style={{
+            opacity: "0.6",
+          }}
+        >
+          Phone: +66 96-930-53xx
+        </Typography>
+        <Typography
+          fontSize={12}
+          color="white"
+          style={{
+            opacity: "0.6",
+          }}
+        >
+          Email: xxxxxx@gmail.com
+        </Typography>
+      </VStack>
+      <VStack
+        width={"10%"}
+        justifyContent={"space-around"}
+        height={"50%"}
+        marginTop={"4rem"}
+      >
+        <Typography fontWeight={fontWeights.bold} fontSize={15} color="#F3C766">
+          Quick Links
+        </Typography>
+        {navigations.map((nav) => (
+          <Link key={nav.path} href={nav.path} passHref>
+            <Box>
+              <Typography
+                color="#FFFFFF"
+                fontSize={15}
+                component={motion.p}
+                style={{
+                  cursor: "pointer",
+                  opacity: "0.6",
+                }}
+                whileHover={{
+                  textShadow: "2px 2px 16px yellow",
+                }}
+              >
+                {nav.text}
+              </Typography>
+              {isCurrentPath(nav.path) && (
+                <motion.div
+                  layout
+                  layoutId="divider"
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  transition={{
+                    duration: 0.4,
+                    ease: "easeOut",
+                  }}
+                  style={{
+                    position: "absolute",
+                    top: "100%",
+                    width: "110%",
+                  }}
+                ></motion.div>
+              )}
+            </Box>
+          </Link>
+        ))}
+        <Typography
+          fontSize={15}
+          color="white"
+          style={{
+            opacity: "0.6",
+          }}
+        >
+          My Account
+        </Typography>
+      </VStack>
+      <Box
+        style={{
+          width: "40%",
+        }}
+      >
+        <div
+          style={{
+            width: "90%",
+            aspectRatio: "16/9",
+            backgroundColor: "#C4C4C4",
+          }}
+        />
+      </Box>
+    </HStack>
   );
 };
 
