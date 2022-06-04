3d786a4591e0ea7eb571c360c1d6ecc0d3348314
Nonthapat Kaewamporn <tontan2545@gmail.com>
2022-06-05 00:31:18 +0700

    remove box props from auth header

--- components/Layout/Header/AuthenticatedHeader.tsx
+++ components/Layout/Header/AuthenticatedHeader.tsx
@@ -1,13 +1,12 @@
 import { HStack } from "@components/common";
 import { Typography, IconButton } from "@mui/material";
-import { BoxProps } from "@mui/system";
 import fontWeights from "@utils/fontWeights";
 import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
 import React from "react";
 
-const AuthenticatedHeader = ({ ...props }: BoxProps) => {
+const AuthenticatedHeader = () => {
   return (
-    <HStack {...props}>
+    <HStack gap={3}>
       <Typography
         fontSize={17}
         fontWeight={fontWeights.extraLight}
