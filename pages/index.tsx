060d300877ef3c128f9e52655104c3a4e2aef62a
Nonthapat Kaewamporn <tontan2545@gmail.com>
2022-06-05 00:32:00 +0700

    add gap to home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,18 @@
 import type { NextPage } from "next";
-import { VStack } from "@components/common";
+import { Center, VStack } from "@components/common";
 import Description from "@components/Description";
 import Product from "@components/Product";
 import Showcase from "@components/Showcase";
+import Timeline from "@mui/lab/Timeline";
+import TimelineItem from "@mui/lab/TimelineItem";
+import TimelineSeparator from "@mui/lab/TimelineSeparator";
+import TimelineConnector from "@mui/lab/TimelineConnector";
+import TimelineContent from "@mui/lab/TimelineContent";
+import TimelineDot from "@mui/lab/TimelineDot";
 
 const Home: NextPage = () => {
   return (
-    <VStack margin={"3rem 6rem 0 6rem"}>
+    <VStack margin={"3rem 6rem 0 6rem"} gap={"5rem"}>
       <Description />
       <Showcase />
       <Product />
