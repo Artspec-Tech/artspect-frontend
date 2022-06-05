5109bf99edfaa2234a3095f573e0aa659bcb0314
Nonthapat Kaewamporn <tontan2545@gmail.com>
2022-06-05 16:30:14 +0700

    add responsive and move to folder

--- components/Layout/Header/AuthHeader/AuthenticatedHeader.tsx
+++ components/Layout/Header/AuthHeader/AuthenticatedHeader.tsx
@@ -1,11 +1,27 @@
 import { HStack } from "@components/common";
-import { Typography, IconButton } from "@mui/material";
+import { Typography, IconButton, useTheme, useMediaQuery } from "@mui/material";
 import fontWeights from "@utils/fontWeights";
 import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
 import React from "react";
 
 const AuthenticatedHeader = () => {
-  return (
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+
+  return isMobile ? (
+    <HStack gap={1}>
+      <Typography
+        fontSize={12}
+        fontWeight={fontWeights.extraLight}
+        whiteSpace="nowrap"
+      >
+        My Account
+      </Typography>
+      <IconButton color="primary" size="small">
+        <ShoppingCartOutlinedIcon />
+      </IconButton>
+    </HStack>
+  ) : (
     <HStack gap={3}>
       <Typography
         fontSize={17}
