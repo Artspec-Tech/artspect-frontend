a7549075f066e01c2d231b6b0b6fbb363ce01f4c
Nonthapat Kaewamporn <tontan2545@gmail.com>
2022-06-05 16:31:17 +0700

    responsive description

--- components/Description/index.tsx
+++ components/Description/index.tsx
@@ -4,14 +4,21 @@ import DescriptionContent from "./DescriptionContent";
 
 const Description = () => {
   return (
-    <VStack gap={"65px"}>
+    <VStack
+      sx={{
+        gap: {
+          xs: "30px",
+          md: "65px",
+        },
+      }}
+    >
       <DescriptionContent
         subTopic="VALUE"
         text="Artspect aims to guide you through the journey of self-exploration using art as a tool. We want to remind people to take a brief pause in their busy schedules, listen to their bodies and emotions, reflect on them, and identify how they are feeling right now. We welcome and invite everyone to join us on this journey."
       />
       <DescriptionContent
         subTopic="PURPOSE"
-        text="We encourage everyone to create their art through which the emotions are expressed. Then we will host an exhibition to showcase everyone’s work. Through this process, we hope to empower poeple to be more expressive of their emotions and inspire people to continue practicing self-expression as their habits."
+        text="We encourage everyone to create their art through which the emotions are expressed. Then we will host an exhibition to showcase everyones work. Through this process, we hope to empower poeple to be more expressive of their emotions and inspire people to continue practicing self-expression as their habits."
       />
     </VStack>
   );
