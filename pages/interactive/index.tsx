505bf8c62e4ec6c85a2d2d72943a1b660ccdeb7c
Nonthapat Kaewamporn <tontan2545@gmail.com>
2022-06-15 01:17:42 +0700

    interactive page

--- pages/interactive/index.tsx
+++ pages/interactive/index.tsx
@@ -1,9 +1,22 @@
-import { Typography } from "@mui/material";
-import { NextPage } from "next";
-import React from "react";
+import { Center } from "@components/common";
+import InteractiveLayout from "@components/Layout/Interactive";
+import { Button } from "@mui/material";
+import { useRouter } from "next/router";
+import React, { ReactElement } from "react";
+import { NextPageWithLayout } from "types";
 
-const Interactive: NextPage = () => {
-  return <Typography>Interactive</Typography>;
+const Interactive: NextPageWithLayout = () => {
+  const router = useRouter();
+  console.log(router.pathname);
+  return (
+    <Center>
+      <Button onClick={() => router.push("/interactive/0")}>Next page</Button>
+    </Center>
+  );
 };
+
+Interactive.getLayout = (page: ReactElement) => (
+  <InteractiveLayout>{page}</InteractiveLayout>
+);
 
 export default Interactive;
