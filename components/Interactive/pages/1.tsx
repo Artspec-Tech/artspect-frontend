6fc9d9a85358d55145311b1406fc5c98c9d6ac19
Nonthapat Kaewamporn <tontan2545@gmail.com>
2022-06-15 01:17:26 +0700

    example interactive page

---
+++ components/Interactive/pages/1.tsx
@@ -0,0 +1,8 @@
+import { Center } from "@components/common";
+import React from "react";
+
+const InteractivePage = () => {
+  return <Center>First page</Center>;
+};
+
+export default InteractivePage;
---
+++ components/Interactive/pages/2.tsx
@@ -0,0 +1,8 @@
+import { Center } from "@components/common";
+import React from "react";
+
+const InteractivePage = () => {
+  return <Center>Second Page</Center>;
+};
+
+export default InteractivePage;
