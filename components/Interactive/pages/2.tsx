983d9563752d7caed461ca9c9f3871db92971660
Parin Opas <ryn.parin@gmail.com>
2022-06-16 19:58:49 +0700

    added page2

--- components/Interactive/pages/2.tsx
+++ components/Interactive/pages/2.tsx
@@ -1,8 +1,24 @@
-import { Center } from "@components/common";
-import React from "react";
+import { VStack, ChoiceButton } from '@components/common';
+import { Typography } from '@mui/material';
+import React from 'react';
 
 const InteractivePage = () => {
-  return <Center>Second Page</Center>;
+	return (
+		<VStack>
+			<Typography
+				variant="h3"
+				align="center"
+				width={'60%'}
+				marginBottom={8}
+				sx={{ fontSize: 20, fontWeight: 'bold' }}
+			>
+				แล้วพบว่ามีหมอกแห่งอารมณ์ เคลื่อนตัวเข้าปกคลุม
+				หมอกเหล่านั้นมีหน้าตา……
+			</Typography>
+			<ChoiceButton idx={0}>หนาปิดมิดจนฉันหา ทางออกไม่ได้</ChoiceButton>
+			<ChoiceButton idx={1}>จางๆพอมีแสงรำไรให้เห็นอยู่บ้าง</ChoiceButton>
+		</VStack>
+	);
 };
 
 export default InteractivePage;
