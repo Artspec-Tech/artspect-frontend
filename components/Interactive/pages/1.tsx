d19ffa70ecb0d5d1de28fd1e3efa6c9829df54bc
Parin Opas <ryn.parin@gmail.com>
2022-06-19 01:15:47 +0700

    Test background video

--- components/Interactive/pages/1.tsx
+++ components/Interactive/pages/1.tsx
@@ -6,30 +6,48 @@ import { CallServiceImage } from '@utils/images';
 
 const InteractivePage = () => {
 	return (
-		<Center>
-			<Typography
-				paddingX={10}
-				sx={{
-					fontSize: 20,
-					fontWeight: 'bold',
-					textAlign: 'center',
-					textShadow: '0px 1px 1px rgba(0, 0, 0, 0.3)',
-				}}
-			>
-				เช้าวันนึงที่แสนจะธรรมดา ฉันตื่นลืมตาขึ้นมาบนเตียง...
-			</Typography>
-			<Box
-				height={'10rem'}
-				position="absolute"
-				bottom={0}
-				left={0}
+		<>
+			<video
+				loop
+				muted
+				autoPlay
 				style={{
-					aspectRatio: '1',
+					width: 'min(100%, 480px)',
+					float: 'left',
+					top: 0,
+					padding: 'none',
+					position: 'fixed',
+					zIndex: -1,
 				}}
 			>
-				<Image src={CallServiceImage} alt="woman sit on a gear" />
-			</Box>
-		</Center>
+				<source src="./videos/slide1.mp4" type="video/mp4" />
+			</video>
+
+			<Center>
+				<Typography
+					paddingX={10}
+					sx={{
+						fontSize: 20,
+						fontWeight: 'bold',
+						textAlign: 'center',
+						textShadow: '0px 1px 1px rgba(0, 0, 0, 0.3)',
+					}}
+				>
+					เช้าวันนึงที่แสนจะธรรมดา ฉันตื่นลืมตาขึ้นมาบนเตียง...
+				</Typography>
+				<Box
+					height={'10rem'}
+					position="absolute"
+					bottom={0}
+					left={0}
+					style={{
+						aspectRatio: '1',
+					}}
+				>
+					<Image src={CallServiceImage} alt="woman sit on a gear" />
+				</Box>
+			</Center>
+		</>
 	);
 };
 
