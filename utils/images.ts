9998742fc56a12c95dff46007b518aa23a43fe1d
Parin Opas <ryn.parin@gmail.com>
2022-06-19 00:08:53 +0700

    Fix call service image

--- utils/images.ts
+++ utils/images.ts
@@ -1,3 +1,3 @@
 export * as FooterBackground from '../public/images/footer-background.png';
 export * as VanGoghImage from '../public/images/van-gogh.jpeg';
-export * as CallServiceImage from '../public/images/Call-Service.jpg';
+export * as CallServiceImage from '../public/images/call-service.png';
