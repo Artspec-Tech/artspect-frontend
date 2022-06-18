e9dbb53da70312cdd6dbd8ba694488c5138a5bc3
Parin Opas <ryn.parin@gmail.com>
2022-06-18 20:41:47 +0700

    Add Inter font

--- components/Interactive/pages/1.tsx
+++ components/Interactive/pages/1.tsx
@@ -1,18 +1,21 @@
 import { Center } from '@components/common';
+import { Typography } from '@mui/material';
 import React from 'react';
 
 const InteractivePage = () => {
 	return (
-		<Center
-			paddingX={10}
-			sx={{
-				fontSize: 20,
-				fontWeight: 'bold',
-				textAlign: 'center',
-				textShadow: '0px 1px 1px rgba(0, 0, 0, 0.3)',
-			}}
-		>
-			เช้าวันนึงที่แสนจะธรรมดา ฉันตื่นลืมตาขึ้นมาบนเตียง...
+		<Center>
+			<Typography
+				paddingX={10}
+				sx={{
+					fontSize: 20,
+					fontWeight: 'bold',
+					textAlign: 'center',
+					textShadow: '0px 1px 1px rgba(0, 0, 0, 0.3)',
+				}}
+			>
+				เช้าวันนึงที่แสนจะธรรมดา ฉันตื่นลืมตาขึ้นมาบนเตียง...
+			</Typography>
 		</Center>
 	);
 };
--- styles/theme.ts
+++ styles/theme.ts
@@ -1,28 +1,41 @@
-import { createTheme } from "@mui/material/styles";
+import { createTheme } from '@mui/material/styles';
 const theme = createTheme({
-  typography: {
-    fontFamily: "Archivo",
-    h1: {
-      fontFamily: "Montserrat",
-    },
-    h2: {
-      fontFamily: "Montserrat",
-    },
-    h3: {
-      fontFamily: "Montserrat",
-    },
-    h4: {
-      fontFamily: "Montserrat",
-    },
-  },
-  palette: {
-    primary: {
-      main: "#000",
-    },
-    secondary: {
-      main: "#FFFFFF",
-    },
-  },
+	typography: {
+		// fontFamily: 'Archivo',
+		// h1: {
+		// 	fontFamily: 'Montserrat',
+		// },
+		// h2: {
+		// 	fontFamily: 'Montserrat',
+		// },
+		// h3: {
+		// 	fontFamily: 'Montserrat',
+		// },
+		// h4: {
+		// 	fontFamily: 'Montserrat',
+		// },
+		fontFamily: 'Inter',
+		h1: {
+			fontFamily: 'Inter',
+		},
+		h2: {
+			fontFamily: 'Inter',
+		},
+		h3: {
+			fontFamily: 'Inter',
+		},
+		h4: {
+			fontFamily: 'Inter',
+		},
+	},
+	palette: {
+		primary: {
+			main: '#000',
+		},
+		secondary: {
+			main: '#FFFFFF',
+		},
+	},
 });
 
 export default theme;
