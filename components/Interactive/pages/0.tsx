15a1e238761ecb3d9b2e8a182646f34b215590ef
Nonthapat Kaewamporn <tontan2545@gmail.com>
2022-06-19 11:25:48 +0700

    POC page 0

--- components/Interactive/pages/0.tsx
+++ components/Interactive/pages/0.tsx
@@ -1,48 +1,40 @@
-import { VStack } from '@components/common';
-import { Typography, Box } from '@mui/material';
-import React from 'react';
+import React, { useEffect, useRef, useState } from "react";
+import { Box } from "@mui/material";
 
 const InteractivePage = () => {
-	return (
-		<VStack>
-			<Box
-				height={':10px'}
-				width={'150%'}
-				sx={{
-					position: 'absolute',
-					border: '0.001px solid #000000',
-					transform: 'rotate(-61.32deg)',
-				}}
-			></Box>
-			<Typography
-				align="center"
-				sx={{
-					fontSize: 50,
-					textTransform: 'uppercase',
-					fontWeight: 'bold',
-					background:
-						'-webkit-linear-gradient(0deg, #6E5684 0%, #EF7F5E 20%, #F3B266 33.09%, #E8BC65 37.11%, #E8BC65 44.21%, #E8BC65 48.36%, #EF7F5E 57.07%, #6E5684 75.42%)',
-
-					WebkitBackgroundClip: 'text',
-					WebkitTextFillColor: 'transparent',
-					textShadow: '0px 4px 4px rgba(0, 0, 0, 0.1)',
-				}}
-			>
-				museum of emotions
-			</Typography>
-			<Typography
-				align="center"
-				sx={{
-					fontSize: 20,
-					fontWeight: 'bold',
-					textShadow: '0px 2px 3px rgba(0, 0, 0, 0.3)',
-					WebkitTextStroke: '0.4px white',
-				}}
-			>
-				By Artspect
-			</Typography>
-		</VStack>
-	);
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const [loop, setLoop] = useState(false);
+  const url = !loop ? "/videos/pg_0_initial.mp4" : "/videos/pg_0_loop.mp4";
+  useEffect(() => {
+    if (videoRef.current) {
+      videoRef.current.load();
+    }
+  }, [url]);
+  return (
+    <Box width={"100%"} height={"100%"}>
+      <video
+        ref={videoRef}
+        autoPlay
+        loop={loop}
+        muted
+        onEnded={() => {
+          if (!loop) setLoop(true);
+        }}
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          width: "100%",
+          height: "100%",
+          position: "absolute",
+          zIndex: -1,
+          objectFit: "cover",
+        }}
+      >
+        <source src={url} type="video/mp4" />
+      </video>
+    </Box>
+  );
 };
 
 export default InteractivePage;
