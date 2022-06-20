bfe8d9166138a6b9691e9eef8caeced81bbc1da3
Nonthapat Kaewamporn <tontan2545@gmail.com>
2022-06-20 17:56:36 +0700

    add page 6 and page 4 wip

--- components/Interactive/pages/6.tsx
+++ components/Interactive/pages/6.tsx
@@ -1,16 +1,32 @@
 import { Center, VStack } from "@components/common";
 import { Typography } from "@mui/material";
 import React from "react";
-import { BackgroundVideo } from "../common";
+import { BackgroundVideo, ChoiceButton } from "../common";
 
 const InteractivePage = () => {
+  const choices = [
+    "สีเหลืองแห่งความสุข (joy)",
+    "สีม่วงน่าประหลาดใจ (surprise)",
+    "สีฟ้าดูเศร้าซึม (sadness)",
+    "สีแดงแห่งความโกรธ (anger)",
+    "สีส้มปนความกลัว (fear)",
+    "สีเขียวน่าขยะแขยง (disgust)",
+  ];
   return (
     <BackgroundVideo>
-      <Center>
-        <VStack>
-          <Typography>Hello</Typography>
+      <VStack width={"100%"} gap="2rem">
+        <Typography fontSize={20} width="80%" textAlign={"center"}>
+          ฉันจึงเฝ้ามองหมอกแห่งอารมณ์ อยู่ครู่นึง แล้วพบว่า
+          หมอกที่ปกคลุมฉันส่วนใหญ่มีสี…..
+        </Typography>
+        <VStack width={"100%"} height={"auto"} gap="1rem">
+          {choices.map((choice, index) => (
+            <ChoiceButton key={index} idx={index}>
+              {choice}
+            </ChoiceButton>
+          ))}
         </VStack>
-      </Center>
+      </VStack>
     </BackgroundVideo>
   );
 };
