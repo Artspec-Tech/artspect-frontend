1feffa5dbb6c7753ec94d737828ed1d5485b7b07
Nonthapat Kaewamporn <tontan2545@gmail.com>
2022-06-21 15:18:36 +0700

    change to double quote

--- components/Interactive/pages/6.tsx
+++ components/Interactive/pages/6.tsx
@@ -1,55 +1,55 @@
-import { VStack } from '@components/common';
-import { Typography } from '@mui/material';
-import React from 'react';
-import { BackgroundVideo, ChoiceButton } from '../common';
+import { VStack } from "@components/common";
+import { Typography } from "@mui/material";
+import React from "react";
+import { BackgroundVideo, ChoiceButton } from "../common";
 
 const InteractivePage = () => {
-	const choices = [
-		'สีเหลืองแห่งความสุข (joy)',
-		'สีม่วงน่าประหลาดใจ (surprise)',
-		'สีฟ้าดูเศร้าซึม (sadness)',
-		'สีแดงแห่งความโกรธ (anger)',
-		'สีส้มปนความกลัว (fear)',
-		'สีเขียวน่าขยะแขยง (disgust)',
-	];
+  const choices = [
+    "สีเหลืองแห่งความสุข (joy)",
+    "สีม่วงน่าประหลาดใจ (surprise)",
+    "สีฟ้าดูเศร้าซึม (sadness)",
+    "สีแดงแห่งความโกรธ (anger)",
+    "สีส้มปนความกลัว (fear)",
+    "สีเขียวน่าขยะแขยง (disgust)",
+  ];
 
-	const choice2color = [
-		'yellow_loop',
-		'purple_loop',
-		'blue_loop',
-		'red_loop',
-		'orange_loop',
-		'green_loop',
-	];
+  const choice2color = [
+    "yellow_loop",
+    "purple_loop",
+    "blue_loop",
+    "red_loop",
+    "orange_loop",
+    "green_loop",
+  ];
 
-	function handleClick(event: React.SyntheticEvent, key: number) {
-		window.localStorage.setItem(
-			'userChoicePage6',
-			JSON.stringify([choice2color[key], choices[key].split(' ')[0]])
-		);
-	}
+  function handleClick(event: React.SyntheticEvent, key: number) {
+    window.localStorage.setItem(
+      "userChoicePage6",
+      JSON.stringify([choice2color[key], choices[key].split(" ")[0]])
+    );
+  }
 
-	return (
-		<BackgroundVideo>
-			<VStack width={'100%'} gap="2rem">
-				<Typography fontSize={20} width="80%" textAlign={'center'}>
-					ฉันจึงเฝ้ามองหมอกแห่งอารมณ์ อยู่ครู่นึง แล้วพบว่า
-					หมอกที่ปกคลุมฉันส่วนใหญ่มีสี…..
-				</Typography>
-				<VStack width={'100%'} height={'auto'} gap="1rem">
-					{choices.map((choice, index) => (
-						<ChoiceButton
-							key={index}
-							idx={index}
-							onClick={(e) => handleClick(e, index)}
-						>
-							{choice}
-						</ChoiceButton>
-					))}
-				</VStack>
-			</VStack>
-		</BackgroundVideo>
-	);
+  return (
+    <BackgroundVideo>
+      <VStack width={"100%"} gap="2rem">
+        <Typography fontSize={20} width="80%" textAlign={"center"}>
+          ฉันจึงเฝ้ามองหมอกแห่งอารมณ์ อยู่ครู่นึง แล้วพบว่า
+          หมอกที่ปกคลุมฉันส่วนใหญ่มีสี…..
+        </Typography>
+        <VStack width={"100%"} height={"auto"} gap="1rem">
+          {choices.map((choice, index) => (
+            <ChoiceButton
+              key={index}
+              idx={index}
+              onClick={(e) => handleClick(e, index)}
+            >
+              {choice}
+            </ChoiceButton>
+          ))}
+        </VStack>
+      </VStack>
+    </BackgroundVideo>
+  );
 };
 
 export default InteractivePage;
--- components/Interactive/pages/8.tsx
+++ components/Interactive/pages/8.tsx
@@ -1,102 +1,95 @@
-import { VStack } from '@components/common';
-import { Typography } from '@mui/material';
-import React from 'react';
-import { BackgroundVideo } from '../common';
-import { styled } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
+import { VStack } from "@components/common";
+import { Typography } from "@mui/material";
+import React from "react";
+import { BackgroundVideo } from "../common";
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
 
 const CssTextField = styled(TextField)({
-	'& label.Mui-focused': {
-		color: '#DED4EC',
-	},
-	'& .MuiInput-underline:after': {
-		borderBottomColor: '#DED4EC',
-	},
-	'& .MuiOutlinedInput-root': {
-		'&:hover fieldset': {
-			borderColor: '#DED4EC',
-		},
-		'&.Mui-focused fieldset': {
-			borderColor: '#DED4EC',
-		},
-	},
+  "& label.Mui-focused": {
+    color: "#DED4EC",
+  },
+  "& .MuiInput-underline:after": {
+    borderBottomColor: "#DED4EC",
+  },
+  "& .MuiOutlinedInput-root": {
+    "&:hover fieldset": {
+      borderColor: "#DED4EC",
+    },
+    "&.Mui-focused fieldset": {
+      borderColor: "#DED4EC",
+    },
+  },
 });
 
 const InteractivePage = () => {
-	const [value, setValue] = React.useState('Controlled');
+  const [value, setValue] = React.useState("Controlled");
 
-	const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-		setValue(event.target.value);
-	};
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(event.target.value);
+  };
 
-	const [userChoicePage2, userChoicePage6, userChoicePage7] =
-		React.useMemo(() => {
-			return [
-				window.localStorage.getItem('userChoicePage2') || '',
-				JSON.parse(
-					window.localStorage.getItem('userChoicePage6') || ''
-				) || Array(2).fill(null),
-				window.localStorage.getItem('userChoicePage7') || '',
-			];
-		}, []);
+  const [userChoicePage2, userChoicePage6, userChoicePage7] =
+    React.useMemo(() => {
+      return [
+        window.localStorage.getItem("userChoicePage2") || "",
+        JSON.parse(window.localStorage.getItem("userChoicePage6") || "") ||
+          Array(2).fill(null),
+        window.localStorage.getItem("userChoicePage7") || "",
+      ];
+    }, []);
 
-	console.log(userChoicePage6);
+  console.log(userChoicePage6);
 
-	//TODO: If user has not yet made a choice, redirect to page 6
+  //TODO: If user has not yet made a choice, redirect to page 6
 
-	return (
-		<BackgroundVideo
-			page="8"
-			videoType={userChoicePage6[0]}
-			loop
-			muted
-			autoPlay
-		>
-			<VStack gap={3.5}>
-				<Typography
-					component="div"
-					variant="h3"
-					align="center"
-					width={'60%'}
-					sx={{ fontSize: 16, fontWeight: 300 }}
-				>
-					ฉันคิดว่าที่หมอกแห่งอารมณ์
-					<Box
-						display="inline"
-						fontWeight="500"
-					>{` (${userChoicePage6[1]}) `}</Box>
-					ที่
-					<Box
-						display="inline"
-						fontWeight="500"
-					>{` (${userChoicePage2}) `}</Box>
-					และควบแน่นที่
-					<Box
-						display="inline"
-						fontWeight="500"
-					>{` (${userChoicePage7}) `}</Box>
-					คงเป็นเพราะว่า ………
-				</Typography>
-				<Box
-					component="form"
-					sx={{
-						'& .MuiTextField-root': { m: 1, width: 300 },
-					}}
-					noValidate
-					autoComplete="off"
-				>
-					<CssTextField
-						id="filled-multiline-static"
-						hiddenLabel
-						multiline
-						rows={6}
-						placeholder="Enter text..."
-					/>
-				</Box>
-			</VStack>
-		</BackgroundVideo>
-	);
+  return (
+    <BackgroundVideo videoType={userChoicePage6[0]}>
+      <VStack gap={3.5}>
+        <Typography
+          component="div"
+          variant="h3"
+          align="center"
+          width={"60%"}
+          sx={{ fontSize: 16, fontWeight: 300 }}
+        >
+          ฉันคิดว่าที่หมอกแห่งอารมณ์
+          <Box
+            display="inline"
+            fontWeight="500"
+          >{` (${userChoicePage6[1]}) `}</Box>
+          ที่
+          <Box
+            display="inline"
+            fontWeight="500"
+          >{` (${userChoicePage2}) `}</Box>
+          และควบแน่นที่
+          <Box
+            display="inline"
+            fontWeight="500"
+          >{` (${userChoicePage7}) `}</Box>
+          คงเป็นเพราะว่า ………
+        </Typography>
+        <Box
+          component="form"
+          sx={{
+            "& .MuiTextField-root": { m: 1, width: 300 },
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          <CssTextField
+            id="filled-multiline-static"
+            hiddenLabel
+            multiline
+            rows={6}
+            placeholder="Enter text..."
+          />
+        </Box>
+      </VStack>
+    </BackgroundVideo>
+  );
 };
 
 export default InteractivePage;
