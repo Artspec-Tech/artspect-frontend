627271df000ea985bf553f0a4fc26aae8aeb942f
Nonthapat Kaewamporn <tontan2545@gmail.com>
2022-06-21 15:18:28 +0700

    change to doubld quote

--- components/Interactive/pages/2.tsx
+++ components/Interactive/pages/2.tsx
@@ -1,46 +1,45 @@
-import { VStack } from '@components/common';
-import { Typography } from '@mui/material';
-import React from 'react';
-import { ChoiceButton, BackgroundVideo } from '../common';
+import { VStack } from "@components/common";
+import { Typography } from "@mui/material";
+import React from "react";
+import { ChoiceButton, BackgroundVideo } from "../common";
 const InteractivePage = () => {
-	const choices = [
-		'หนาปิดมิดจนฉันหา ทางออกไม่ได้',
-		'จางๆพอมีแสงรำไรให้เห็นอยู่บ้าง',
-	];
+  const choices = [
+    "หนาปิดมิดจนฉันหา ทางออกไม่ได้",
+    "จางๆพอมีแสงรำไรให้เห็นอยู่บ้าง",
+  ];
 
-	function handleClick(event: React.SyntheticEvent, key: number) {
-		window.localStorage.setItem('userChoicePage2', choices[key]);
-	}
+  function handleClick(event: React.SyntheticEvent, key: number) {
+    window.localStorage.setItem("userChoicePage2", choices[key]);
+  }
 
-	return (
-		<BackgroundVideo>
-			<VStack gap={3.5}>
-				<Typography
-					variant="h3"
-					align="center"
-					width={'60%'}
-					marginBottom={8}
-					sx={{
-						fontSize: 20,
-						fontWeight: 'bold',
-						letterSpacing: '1px',
-					}}
-				>
-					แล้วพบว่ามีหมอกแห่งอารมณ์ เคลื่อนตัวเข้าปกคลุม
-					หมอกเหล่านั้นมีหน้าตา……
-				</Typography>
-				{choices.map((choice, index) => (
-					<ChoiceButton
-						key={index}
-						idx={index}
-						onClick={(e) => handleClick(e, index)}
-					>
-						{choice}
-					</ChoiceButton>
-				))}
-			</VStack>
-		</BackgroundVideo>
-	);
+  return (
+    <BackgroundVideo>
+      <VStack gap={3.5}>
+        <Typography
+          variant="h3"
+          align="center"
+          width={"60%"}
+          marginBottom={8}
+          sx={{
+            fontSize: 20,
+            fontWeight: "bold",
+            letterSpacing: "1px",
+          }}
+        >
+          แล้วพบว่ามีหมอกแห่งอารมณ์ เคลื่อนตัวเข้าปกคลุม หมอกเหล่านั้นมีหน้าตา……
+        </Typography>
+        {choices.map((choice, index) => (
+          <ChoiceButton
+            key={index}
+            idx={index}
+            onClick={(e) => handleClick(e, index)}
+          >
+            {choice}
+          </ChoiceButton>
+        ))}
+      </VStack>
+    </BackgroundVideo>
+  );
 };
 
 export default InteractivePage;
