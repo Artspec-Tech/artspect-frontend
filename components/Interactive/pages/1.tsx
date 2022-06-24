3f7f0ff2b304471089061b79240f691afb1edbcb
Parin Opas <ryn.parin@gmail.com>
2022-06-24 14:29:39 +0700

    fix fade in page 1

--- components/Interactive/pages/1.tsx
+++ components/Interactive/pages/1.tsx
@@ -4,33 +4,11 @@ import React from 'react';
 import BackgroundVideo from '../common/BackgroundVideo';
 import { motion } from 'framer-motion';
 
-const stagger = {
-	animate: {
-		transition: {
-			staggerChildren: 0.05,
-		},
-	},
-};
-
-const fadeIn = {
-	initial: {
-		opacity: 0,
-	},
-	animate: {
-		opacity: 1,
-		transition: {
-			delay: 10,
-			duaration: 6,
-		},
-	},
-};
-
 const InteractivePage = () => {
 	return (
 		<BackgroundVideo loop={false}>
 			<Center>
 				<Typography
-					component={motion.h1}
 					paddingX={10}
 					sx={{
 						fontSize: 20,
@@ -38,9 +16,13 @@ const InteractivePage = () => {
 						textAlign: 'center',
 						textShadow: '0px 1px 1px rgba(0, 0, 0, 0.3)',
 					}}
-					variants={fadeIn}
-					initial="initial"
-					animate="animate"
+					component={motion.h1}
+					initial={{ opacity: 0 }}
+					animate={{ opacity: 1 }}
+					transition={{
+						delay: 10,
+						duration: 5,
+					}}
 				>
 					เช้าวันนึงที่แสนจะธรรมดา ฉันตื่นลืมตาขึ้นมาบนเตียง...
 				</Typography>
