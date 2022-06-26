ad869dd0ebb816a5c0c65315ee8e52eaa79fb035
Nonthapat Kaewamporn <tontan2545@gmail.com>
2022-06-26 17:47:19 +0700

    remove text on two page

--- components/Interactive/pages/4.tsx
+++ components/Interactive/pages/4.tsx
@@ -1,34 +1,8 @@
-import { Center, VStack } from '@components/common';
-import { Typography } from '@mui/material';
-import React from 'react';
-import { BackgroundVideo } from '../common';
+import React from "react";
+import { BackgroundVideo } from "../common";
 
 const InteractivePage = () => {
-	return (
-		<BackgroundVideo>
-			<Center>
-				<VStack>
-					<Typography
-						sx={{
-							width: '50%',
-							textAlign: 'right',
-							position: 'absolute',
-							top: '1.5rem',
-							left: '10rem',
-						}}
-					>
-						หมอกสีเหลืองแสดงถึงความสุข สงบ พอใจ และสนุกสนาน
-					</Typography>
-					<Typography>
-						หมอกสีม่วงน่าประหลาดใจ ตื่นเต้น และสับสน
-					</Typography>
-					<Typography>
-						หมอกสีฟ้า ดูเศร้าซึม เหงาๆ และสิ้นหวัง
-					</Typography>
-				</VStack>
-			</Center>
-		</BackgroundVideo>
-	);
+  return <BackgroundVideo />;
 };
 
 export default InteractivePage;
