e5cd9d9e5d8fb341249520d48da1ff558d15d36b
leilasigl <leilasigles@gmail.com>
2022-06-27 17:16:22 +0700

    fix: width on choice buttons in page 16

--- components/Interactive/pages/16.tsx
+++ components/Interactive/pages/16.tsx
@@ -6,25 +6,23 @@ import { ChoiceButton, BackgroundVideo } from '../common';
 const InteractivePage = () => {
 	return (
 		<BackgroundVideo page="3" videoType="loop" loop muted autoPlay>
-			<Center>
-				<VStack gap={4}>
-					<Typography
-						align='center'
-            fontSize={20}
-          >
-            ระหว่างที่กำลังเต้นอยู่นั้น หมอกก็ถามฉันว่า<br />“เป็นยังไงบ้าง”
-          </Typography>
-					<ChoiceButton idx={0}>
-						สนุกมากเลย!<br />ได้รับพลังบวกของพวกเขาเข้ามา<br />เต็มเปี่ยมเลยแหละ
-          </ChoiceButton>
-          <ChoiceButton idx={1}>
-						มันก็ดีนะ<br />ฉันไม่ได้ปลดปล่อยแบบนี้มานานแล้ว
-          </ChoiceButton>
-          <ChoiceButton idx={2}>
-						ก็โอเคนะ<br />แต่คราวหน้าขอดูอย่างเดียวดีกว่า
-          </ChoiceButton>
-				</VStack>
-			</Center>
+			<VStack gap={4}>
+				<Typography
+					align='center'
+					fontSize={20}
+				>
+					ระหว่างที่กำลังเต้นอยู่นั้น หมอกก็ถามฉันว่า<br />“เป็นยังไงบ้าง”
+				</Typography>
+				<ChoiceButton idx={0}>
+					สนุกมากเลย! ได้รับพลังบวกของพวกเขาเข้ามา เต็มเปี่ยมเลยแหละ
+				</ChoiceButton>
+				<ChoiceButton idx={1}>
+					มันก็ดีนะ ฉันไม่ได้ปลดปล่อยแบบนี้มานานแล้ว
+				</ChoiceButton>
+				<ChoiceButton idx={2}>
+					ก็โอเคนะ แต่คราวหน้าขอดูอย่างเดียวดีกว่า
+				</ChoiceButton>
+			</VStack>
 		</BackgroundVideo>
 	);
 };
