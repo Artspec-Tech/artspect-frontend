22fcc059be4247b00d290dbef43c261af17fe79d
Nonthapat Kaewamporn <tontan2545@gmail.com>
2022-06-28 01:29:46 +0700

    update artists

--- components/Layout/Web/Footer/artists.ts
+++ components/Layout/Web/Footer/artists.ts
@@ -1,4 +1,9 @@
-import { VanGoghImage } from "@utils/images";
+import {
+  AristotleImage,
+  MathioleImage,
+  VanGoghImage,
+  WinnicottImage,
+} from "@utils/images";
 
 const artists = [
   {
@@ -8,10 +13,21 @@ const artists = [
     name: "Vincent Van Gogh",
   },
   {
-    img: VanGoghImage,
+    img: WinnicottImage,
     quote:
-      '"Paintings have a life of their own that derives from the painter\'s soul."',
-    name: "Vincent Van Gogh",
+      '"It is only in being creative that the individual discovers the self."',
+    name: "D.W. Winnicott",
+  },
+  {
+    img: MathioleImage,
+    quote: '"Art speaks where words are unable to explain"',
+    name: "Mathiole",
+  },
+  {
+    img: AristotleImage,
+    quote:
+      '"The aim of art is to represent not the outward appearance of things, but their inward significance."',
+    name: "Aristotle",
   },
 ];
 
