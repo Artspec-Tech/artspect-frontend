1869e2c5f97061eaf536fe3114176194ffe00d34
Ryn Parin <ryn.parin@gmail.com>
2022-06-28 15:43:10 +0700

    Added page 32, 33

--- components/Interactive/pages/32.tsx
+++ components/Interactive/pages/32.tsx
@@ -1,8 +1,37 @@
+import { VStack } from '@components/common';
+import { Typography, TypographyProps } from '@mui/material';
 import React from 'react';
-import { BackgroundVideo } from '../common';
+import BackgroundVideo from '../common/BackgroundVideo';
 
 const InteractivePage = () => {
-	return <BackgroundVideo />;
+	return (
+		<BackgroundVideo>
+			<VStack gap={6}>
+				<Typography
+					paddingX={8}
+					sx={{
+						fontSize: 20,
+						fontWeight: 'bold',
+						textAlign: 'center',
+						textShadow: '0px 1px 1px rgba(0, 0, 0, 0.3)',
+					}}
+				>
+					“สิ่งนี้อาจเป็นสิ่งที่เธออยากลองทำลึกๆมา ตลอดก็ได้”
+				</Typography>
+				<Typography
+					paddingX={8}
+					sx={{
+						fontSize: 20,
+						fontWeight: 'bold',
+						textAlign: 'center',
+						textShadow: '0px 1px 1px rgba(0, 0, 0, 0.3)',
+					}}
+				>
+					“ถ้าเวลาในวันนี้หมดลงแล้ว ลองทำมันดูสิ”
+				</Typography>
+			</VStack>
+		</BackgroundVideo>
+	);
 };
 
 export default InteractivePage;
--- components/Interactive/pages/33.tsx
+++ components/Interactive/pages/33.tsx
@@ -1,8 +1,38 @@
+import { VStack } from '@components/common';
+import { Typography, TypographyProps } from '@mui/material';
 import React from 'react';
-import { BackgroundVideo } from '../common';
+import BackgroundVideo from '../common/BackgroundVideo';
 
 const InteractivePage = () => {
-	return <BackgroundVideo />;
+	return (
+		<BackgroundVideo>
+			<VStack gap={6}>
+				<Typography
+					paddingX={8}
+					sx={{
+						fontSize: 20,
+						fontWeight: 'bold',
+						textAlign: 'center',
+						textShadow: '0px 1px 1px rgba(0, 0, 0, 0.3)',
+					}}
+				>
+					ภายในห้องโล่งนี้ มีเพียงแค่ตัวคุณ
+					กับหมอกแห่งอารมณ์ที่เดินทางเป็นเพื่อนคุณมาตลอดในวันนี้
+				</Typography>
+				<Typography
+					paddingX={8}
+					sx={{
+						fontSize: 20,
+						fontWeight: 'bold',
+						textAlign: 'center',
+						textShadow: '0px 1px 1px rgba(0, 0, 0, 0.3)',
+					}}
+				>
+					หรืออาจจะอยู่มาตลอดชีวิต โดยที่คุณไม่เคยรู้ตัวเลยก็ได้
+				</Typography>
+			</VStack>
+		</BackgroundVideo>
+	);
 };
 
 export default InteractivePage;
