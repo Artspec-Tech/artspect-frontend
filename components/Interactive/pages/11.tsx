d1388633fc5432fc6047ba26ad6eb76e3a34e343
Ryn Parin <ryn.parin@gmail.com>
2022-06-29 21:35:14 +0700

    minor change xD

--- components/Interactive/pages/11.tsx
+++ components/Interactive/pages/11.tsx
@@ -1,36 +1,29 @@
-import { Center, VStack } from "@components/common";
-import { Typography } from "@mui/material";
-import React from "react";
-import BackgroundVideo from "../common/BackgroundVideo";
+import { Center, VStack } from '@components/common';
+import { Typography } from '@mui/material';
+import React from 'react';
+import BackgroundVideo from '../common/BackgroundVideo';
 
 const InteractivePage = () => {
-  return (
-    <BackgroundVideo loop={false}>
-      <Center>
-        <VStack>
-          <Typography
-            sx={{
-              fontSize: 18,
-              textAlign: "center",
-              textShadow: "0px 1px 1px rgba(0, 0, 0, 0.3)",
-              marginBottom: 2,
-            }}
-          >
-            &quot;ฉันอยากจะพาเธอไปยังที่แห่งหนึ่ง
-          </Typography>
-          <Typography
-            sx={{
-              fontSize: 18,
-              textAlign: "center",
-              textShadow: "0px 1px 1px rgba(0, 0, 0, 0.3)",
-            }}
-          >
-            ตามฉันมานะ&quot;
-          </Typography>
-        </VStack>
-      </Center>
-    </BackgroundVideo>
-  );
+	return (
+		<BackgroundVideo loop={false}>
+			<Center>
+				<VStack>
+					<Typography
+						sx={{
+							fontSize: 18,
+							textAlign: 'center',
+							textShadow: '0px 1px 1px rgba(0, 0, 0, 0.3)',
+							marginBottom: 2,
+						}}
+					>
+						“ฉันอยากจะพาเธอไปยังที่แห่งหนึ่ง
+						<br />
+						ตามฉันมานะ”
+					</Typography>
+				</VStack>
+			</Center>
+		</BackgroundVideo>
+	);
 };
 
 export default InteractivePage;
--- components/Interactive/pages/12.tsx
+++ components/Interactive/pages/12.tsx
@@ -1,36 +1,38 @@
-import { Center, VStack } from "@components/common";
-import { Typography, TypographyProps } from "@mui/material";
-import React from "react";
-import BackgroundVideo from "../common/BackgroundVideo";
+import { Center, VStack } from '@components/common';
+import { Typography, TypographyProps } from '@mui/material';
+import React from 'react';
+import BackgroundVideo from '../common/BackgroundVideo';
 const PageTypography = ({ children, ...props }: TypographyProps) => {
-  return (
-    <Typography
-      sx={{
-        fontSize: 18,
-        textAlign: "center",
-        textShadow: "0px 1px 1px rgba(0, 0, 0, 0.3)",
-      }}
-      {...props}
-    >
-      {children}
-    </Typography>
-  );
+	return (
+		<Typography
+			sx={{
+				fontSize: 18,
+				textAlign: 'center',
+				textShadow: '0px 1px 1px rgba(0, 0, 0, 0.3)',
+			}}
+			{...props}
+		>
+			{children}
+		</Typography>
+	);
 };
 const InteractivePage = () => {
-  return (
-    <BackgroundVideo>
-      <VStack justifyContent={"start"}>
-        <PageTypography marginTop={5} marginBottom={2}>
-          &quot;ยินดีต้อนรับสู่ (museum of emotions) นะ
-        </PageTypography>
-        <PageTypography>ที่นี่น่ะ</PageTypography>
-        <PageTypography marginBottom={3}>
-          ทุกคนมีหมอกของตัวเองมาด้วยกันทั้งนั้น
-        </PageTypography>
-        <PageTypography>แล้วก็มีหลายสีเต็มไปหมดเลย&quot;</PageTypography>
-      </VStack>
-    </BackgroundVideo>
-  );
+	return (
+		<BackgroundVideo>
+			<VStack justifyContent={'start'}>
+				<PageTypography marginTop={5} marginBottom={2}>
+					&quot;ยินดีต้อนรับสู่ (museum of emotions) นะ
+				</PageTypography>
+				<PageTypography>ที่นี่น่ะ</PageTypography>
+				<PageTypography marginBottom={3}>
+					ทุกคนมีหมอกของตัวเองมาด้วยกันทั้งนั้น
+				</PageTypography>
+				<PageTypography>
+					แล้วก็มีหลายสีเต็มไปหมดเลย&quot;
+				</PageTypography>
+			</VStack>
+		</BackgroundVideo>
+	);
 };
 
 export default InteractivePage;
