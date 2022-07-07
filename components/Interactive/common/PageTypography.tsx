8421a96885e488ccc7fe0d81dffa9d2dab8311b5
Nonthapat Kaewamporn <tontan2545@gmail.com>
2022-07-08 04:39:06 +0700

    more generic page type

--- components/Interactive/common/PageTypography.tsx
+++ components/Interactive/common/PageTypography.tsx
@@ -1,32 +1,42 @@
-import { Typography, TypographyProps } from '@mui/material';
-import { motion, MotionProps } from 'framer-motion';
+import { Typography, TypographyProps } from "@mui/material";
+import { motion, MotionProps } from "framer-motion";
+
+type Props = {
+  delay?: number;
+  duration?: number;
+  fontWeight?: TypographyProps["fontWeight"];
+  fontSize?: number;
+  children: React.ReactNode;
+};
+
 const PageTypography = ({
-	delay = 1,
-	duration = 3,
-	children,
-	...props
-}: any) => {
-	return (
-		<Typography
-			component={motion.h3}
-			paddingX={10}
-			fontSize={20}
-			sx={{
-				fontWeight: 'bold',
-				textAlign: 'center',
-				textShadow: '0px 1px 1px rgba(0, 0, 0, 0.3)',
-			}}
-			initial={{ opacity: 0 }}
-			animate={{ opacity: 1 }}
-			transition={{
-				delay: delay,
-				duration: duration,
-			}}
-			{...props}
-		>
-			{children}
-		</Typography>
-	);
+  delay = 1,
+  duration = 3,
+  fontWeight = 500,
+  fontSize,
+  children,
+}: Props) => {
+  return (
+    <Typography
+      component={motion.h3}
+      paddingX={"1rem"}
+      fontSize={20}
+      fontWeight={fontWeight}
+      sx={{
+        fontSize: fontSize || 18,
+        textAlign: "center",
+        textShadow: "0px 1px 1px rgba(0, 0, 0, 0.3)",
+      }}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{
+        delay: delay,
+        duration: duration,
+      }}
+    >
+      {children}
+    </Typography>
+  );
 };
 
 export default PageTypography;
