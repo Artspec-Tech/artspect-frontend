97a708d84bca5ee179e2eff3f1427b91eacae380
Nonthapat Kaewamporn <tontan2545@gmail.com>
2022-07-08 01:55:21 +0700

    change compression

--- compress.sh
+++ compress.sh
@@ -6,7 +6,7 @@ for file in */*; do
     compressed_file_path="${array[0]}/compressed_${array[1]}"
     if [[ ! -f "$compressed_file_path" ]]; then
         echo "Compressing $file_path to $compressed_file_path"
-        ffmpeg -i "$file_path" -vcodec libx264 -pix_fmt yuv420p -crf 40 -an "$compressed_file_path"
+        ffmpeg -i "$file_path" -vcodec libx264 -pix_fmt yuv420p -crf 37 -an "$compressed_file_path"
         rm "$file_path"
         mv "$compressed_file_path" "$file_path"
     else
