f759f424b13dc41d91cb280dd4b57cc40b788c24
Ryn Parin <ryn.parin@gmail.com>
2022-07-08 19:20:39 +0700

    add bowser to check user browser for render video

--- components/Interactive/common/BackgroundVideo.tsx
+++ components/Interactive/common/BackgroundVideo.tsx
@@ -1,65 +1,78 @@
-import { VStack } from "@components/common";
-import { StackProps } from "@mui/material";
-import { arrayBufferToBlob } from "@utils/binaryOperations";
-import { db, Video } from "@utils/db";
-import { fetchInteractiveVideos } from "@utils/fetchInteractiveVideos";
-import { getOrElse } from "@utils/nullOperations";
-import { useNextPage } from "hooks/useNextPage";
-import { useRouter } from "next/router";
-import React, { MediaHTMLAttributes } from "react";
+import { VStack } from '@components/common';
+import { StackProps } from '@mui/material';
+import { arrayBufferToBlob } from '@utils/binaryOperations';
+import { db, Video } from '@utils/db';
+import { fetchInteractiveVideos } from '@utils/fetchInteractiveVideos';
+import { getOrElse } from '@utils/nullOperations';
+import { useNextPage } from 'hooks/useNextPage';
+import { useRouter } from 'next/router';
+import React, { MediaHTMLAttributes } from 'react';
+import Bowser from 'bowser';
 
 type Props = {
-  page?: string;
-  src?: string;
-  videoType?: string;
-  nextVideoOnClick?: boolean;
-  handleEnded?: () => void;
+	page?: string;
+	src?: string;
+	videoType?: string;
+	nextVideoOnClick?: boolean;
+	handleEnded?: () => void;
 } & MediaHTMLAttributes<any> &
-  StackProps;
+	StackProps;
 
 const BackgroundVideo = ({
-  page,
-  src,
-  videoType = "loop",
-  handleEnded = () => 0,
-  children,
-  loop = true,
-  autoPlay = true,
-  muted = true,
-  nextVideoOnClick = true,
-  ref,
-  ...props
+	page,
+	src,
+	videoType = 'loop',
+	handleEnded = () => 0,
+	children,
+	loop = true,
+	autoPlay = true,
+	muted = true,
+	nextVideoOnClick = true,
+	ref,
+	...props
 }: Props) => {
-  const { query } = useRouter();
-  const nextPage = useNextPage();
-  const page_path = `/videos/interactive/${getOrElse(
-    page,
-    query.page
-  )}/${videoType}.mp4`;
-  return (
-    <VStack
-      onClick={nextVideoOnClick ? nextPage : undefined}
-      ref={ref}
-      {...props}
-    >
-      <video
-        onEnded={() => handleEnded()}
-        style={{
-          width: "100%",
-          height: "100%",
-          position: "absolute",
-          zIndex: -1,
-          objectFit: "cover",
-        }}
-        loop={loop}
-        autoPlay={autoPlay}
-        muted={muted}
-        src={page_path}
-        typeof={"video/mp4"}
-      />
-      {children}
-    </VStack>
-  );
+	const { query } = useRouter();
+	const nextPage = useNextPage();
+
+	let page_path;
+	if (Bowser.safari) {
+		console.log('safari');
+		page_path = `/videos/interactive/safari/${getOrElse(
+			page,
+			query.page
+		)}/${videoType}.mp4`;
+	} else {
+		console.log('chromium');
+		page_path = `/videos/interactive/chromium/${getOrElse(
+			page,
+			query.page
+		)}/${videoType}.mp4`;
+	}
+
+	return (
+		<VStack
+			onClick={nextVideoOnClick ? nextPage : undefined}
+			ref={ref}
+			{...props}
+		>
+			<video
+				onEnded={() => handleEnded()}
+				style={{
+					width: '100%',
+					height: '100%',
+					position: 'absolute',
+					zIndex: -1,
+					objectFit: 'cover',
+				}}
+				loop={loop}
+				autoPlay={autoPlay}
+				muted={muted}
+				src={page_path}
+				typeof={'video/mp4'}
+			/>
+			{children}
+		</VStack>
+	);
 };
 
 export default BackgroundVideo;
