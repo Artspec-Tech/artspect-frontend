058aef6147a1d381bc4f97f64a9edf0178e690ca
Ryn Parin <ryn.parin@gmail.com>
2022-07-08 20:58:07 +0700

    center text page1

--- components/Interactive/pages/1.tsx
+++ components/Interactive/pages/1.tsx
@@ -1,20 +1,20 @@
-import { Center } from "@components/common";
-import React from "react";
-import BackgroundVideo from "../common/BackgroundVideo";
-import PageTypography from "../common/PageTypography";
+import { Center } from '@components/common';
+import React from 'react';
+import BackgroundVideo from '../common/BackgroundVideo';
+import PageTypography from '../common/PageTypography';
 
 const InteractivePage = () => {
-  return (
-    <BackgroundVideo loop={false}>
-      <Center marginBottom={"40rem"}>
-        <PageTypography delay={10} duration={5}>
-          เช้าวันนึงที่แสนจะธรรมดา
-          <br />
-          ฉันตื่นลืมตาขึ้นมาบนเตียง...
-        </PageTypography>
-      </Center>
-    </BackgroundVideo>
-  );
+	return (
+		<BackgroundVideo loop={false}>
+			<Center>
+				<PageTypography delay={10} duration={5}>
+					เช้าวันนึงที่แสนจะธรรมดา
+					<br />
+					ฉันตื่นลืมตาขึ้นมาบนเตียง...
+				</PageTypography>
+			</Center>
+		</BackgroundVideo>
+	);
 };
 
 export default InteractivePage;
