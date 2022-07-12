c444bbcc57fffe56be0399e4af1ef603f382f6ed
Ryn Parin <ryn.parin@gmail.com>
2022-07-12 14:21:12 +0700

    comment out workshop path

--- components/Layout/Web/Header/navigationList.ts
+++ components/Layout/Web/Header/navigationList.ts
@@ -1,30 +1,30 @@
-import React from "react";
-import HomeIcon from "@mui/icons-material/Home";
-import FormatShapesIcon from "@mui/icons-material/FormatShapes";
-import ModelTrainingIcon from "@mui/icons-material/ModelTraining";
-import { SvgIconTypeMap } from "@mui/material";
-import { OverridableComponent } from "@mui/material/OverridableComponent";
+import React from 'react';
+import HomeIcon from '@mui/icons-material/Home';
+import FormatShapesIcon from '@mui/icons-material/FormatShapes';
+import ModelTrainingIcon from '@mui/icons-material/ModelTraining';
+import { SvgIconTypeMap } from '@mui/material';
+import { OverridableComponent } from '@mui/material/OverridableComponent';
 
 type Navigation = {
-  path: string;
-  text: string;
-  icon: OverridableComponent<SvgIconTypeMap> & { muiName: string };
+	path: string;
+	text: string;
+	icon: OverridableComponent<SvgIconTypeMap> & { muiName: string };
 };
 
 export const navigations: Navigation[] = [
-  {
-    path: "/",
-    text: "Home",
-    icon: HomeIcon,
-  },
-  {
-    path: "/artwork",
-    text: "Artwork",
-    icon: FormatShapesIcon,
-  },
-  {
-    path: "/workshop",
-    text: "Workshop",
-    icon: ModelTrainingIcon,
-  },
+	{
+		path: '/',
+		text: 'Home',
+		icon: HomeIcon,
+	},
+	{
+		path: '/artwork',
+		text: 'Artwork',
+		icon: FormatShapesIcon,
+	},
+	// {
+	//   path: "/workshop",
+	//   text: "Workshop",
+	//   icon: ModelTrainingIcon,
+	// },
 ];
